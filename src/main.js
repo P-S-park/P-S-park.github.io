6ff94ba72d6138cdfc1a765ea04b1bacdfea0379
Sergei Sokolov <hello@sergeisokolov.com>
2022-01-31 00:38:39 +0300

    metrika function approach

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import './metrika'
+import metrika from './metrika'
 
 Vue.config.productionTip = false
 
@@ -9,3 +9,5 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+metrika();
--- src/metrika.js
+++ src/metrika.js
@@ -1,8 +1,10 @@
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+export default function() {
+  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-window.ym(87334429, "init", {
-     clickmap:false,
-     trackLinks:false,
-     accurateTrackBounce:false
-});
+  window.ym(87334429, "init", {
+      clickmap:false,
+      trackLinks:false,
+      accurateTrackBounce:false
+  });
+}
