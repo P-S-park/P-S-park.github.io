7f94fa066970530dd04681d339bdbf1afdcadd48
Sergei Sokolov <hello@sergeisokolov.com>
2022-02-05 00:39:03 +0300

    unused articles from tmpl

--- src/data/index.js
+++ src/data/index.js
@@ -1,13 +1,11 @@
 import actions from './actions';
 import appeal from './appeal';
-import articles from './articles';
 import kiosk from './kiosk';
 import stats from './stats';
 
 export default {
   actions,
   appeal,
-  articles,
   kiosk,
   stats,
 };
