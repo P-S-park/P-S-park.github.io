09fdde4def3be86a60ec1676ca19c3dbd6ba194d
Sergei Sokolov <hello@sergeisokolov.com>
2022-02-08 08:21:37 +0300

    Footer added SAO

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,10 +2,11 @@
   <v-footer
     class="justify-center"
     color="#292929"
-    height="130"
+    height="170"
   >
     <div class="title font-weight-light grey--text text--lighten-1 text-center">
-      Россия, Москва, <abbr title="Северо-Западный административный округ">СЗАО</abbr>, район Покровское-Стрешнево
+      Россия, Москва, <abbr title="Северо-Западный административный округ">СЗАО</abbr> 
+      и <abbr title="Северный административный округ">САО</abbr>, район Покровское-Стрешнево
       <br>Контакты:
       <v-btn href="mailto:ps-park@mail.ru" icon dark small ><v-icon dark v-html="mdiAt" /></v-btn>
       <v-btn href="https://www.facebook.com/groups/pipstreshnevo" icon dark small ><v-icon dark v-html="mdiFacebook" /></v-btn>
