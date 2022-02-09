32f1df0b2f6b16226b24ed610472ac82eba0f6ef
Sergei Sokolov <hello@sergeisokolov.com>
2022-02-09 21:34:19 +0300

    Build timestamp

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,6 +10,7 @@
       <br>Контакты:
       <v-btn href="mailto:ps-park@mail.ru" icon dark small ><v-icon dark v-html="mdiAt" /></v-btn>
       <v-btn href="https://www.facebook.com/groups/pipstreshnevo" icon dark small ><v-icon dark v-html="mdiFacebook" /></v-btn>
+      <span v-if="buildString">Обновлено: {{ buildString }}</span>
       <br>&copy; {{ (new Date()).getFullYear() }}, неравнодушные москвичи.
     </div>
   </v-footer>
@@ -23,10 +24,30 @@ import {
 
 export default {
   data() {
+    let buildString;
+    try {
+      const ts = document.documentElement.dataset.buildTimestamp || Date.now();
+      const D = new Date();
+      D.setTime(ts);
+
+      buildString = new Intl.DateTimeFormat(
+        'ru-RU',
+        { month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', }
+      )
+      .format(D);
+    } catch (e) {
+      console.error(e);
+      buildString = false;
+    }
+
     return {
       mdiAt,
       mdiFacebook,
+      buildString,
     };
+  },
+
+  computed: {
   },
 }
 </script>
