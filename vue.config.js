4db010e7163d1174ce63f7509ad196034d12d4e0
Sergei Sokolov <hello@sergeisokolov.com>
2022-02-15 15:31:40 +0300

    Sitemap plugin added

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,21 @@
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+
+const base = 'https://pspark.ru';
+const paths = ['/'];
+
+// https://github.com/schneidmaster/sitemap-webpack-plugin
+const options = {
+  lastmod: true, 
+};
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  outputDir: 'docs'
+  outputDir: 'docs',
+  configureWebpack: {
+    plugins: [
+      new SitemapPlugin({ base, paths, options })
+    ]
+  }
 }
