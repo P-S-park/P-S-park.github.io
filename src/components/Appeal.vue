06c0f8fae5f90f9a2861d1cc3466891fa30eb296
Sergei Sokolov <hello@sergeisokolov.com>
2022-02-16 23:05:27 +0300

    cleanup Appeal component

--- src/components/Appeal.vue
+++ src/components/Appeal.vue
@@ -7,61 +7,61 @@
     :fullscreen="isMobile"
   >
     <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        color="deep-orange"
+        outlined
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon small left v-html="icons.mdiSend" />
+        Отправить
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-toolbar
+        dark
+        color="green"
+      >
         <v-btn
-          color="deep-orange"
-          outlined
-          v-bind="attrs"
-          v-on="on"
-        >
-          <v-icon small left v-html="icons.mdiSend" />
-          Отправить
-        </v-btn>
-      </template>
-
-      <v-card>
-        <v-toolbar
+          icon
           dark
-          color="green"
+          @click="dialog = false"
         >
-          <v-btn
-            icon
-            dark
-            @click="dialog = false"
-          >
-            <v-icon v-html="icons.mdiClose"></v-icon>
+          <v-icon v-html="icons.mdiClose"></v-icon>
+        </v-btn>
+        <v-toolbar-title v-text="title || 'Обращение'" />
+      </v-toolbar>
+      <v-card>
+        <v-card-title>Суть</v-card-title>
+        <v-card-text v-text="brief" />
+
+        <v-card-title>Адресаты</v-card-title>
+        <v-card-text v-html="recepients" />
+          
+        <v-card-title>Текст обращения</v-card-title>
+        <v-divider />
+        <v-card-text class="doctext" v-text="text" />
+        <v-divider />
+
+        <v-card-actions>
+          <CopyButton :text="text" />
+          <v-btn :x-small="$vuetify.breakpoint.smAndDown" outlined color="deep-orange" v-if="urlLetter" :href="urlLetter">
+            <v-icon :x-small="$vuetify.breakpoint.smAndDown" small dark left v-html="icons.mdiDownload" />
+            текст
           </v-btn>
-          <v-toolbar-title v-text="title || 'Обращение'" />
-        </v-toolbar>
-        <v-card>
-          <v-card-title>Суть</v-card-title>
-          <v-card-text v-text="brief" />
-
-          <v-card-title>Адресаты</v-card-title>
-          <v-card-text v-html="recepients" />
-            
-          <v-card-title>Текст обращения</v-card-title>
-          <v-divider />
-          <v-card-text class="doctext" v-text="text" />
-          <v-divider />
-
-          <v-card-actions>
-            <CopyButton :text="text" />
-            <v-btn :x-small="$vuetify.breakpoint.smAndDown" outlined color="deep-orange" v-if="urlLetter" :href="urlLetter">
-              <v-icon :x-small="$vuetify.breakpoint.smAndDown" small dark left v-html="icons.mdiDownload" />
-              текст
-            </v-btn>
-            <v-btn :x-small="$vuetify.breakpoint.smAndDown" outlined color="deep-orange" v-if="urlAttachments" :href="urlAttachments">
-              <v-icon :x-small="$vuetify.breakpoint.smAndDown" small dark left v-html="icons.mdiDownload" />
-              приложения
-            </v-btn>
-            <v-btn :x-small="$vuetify.breakpoint.smAndDown" outlined color="deep-orange" v-if="urlPdf" :href="urlPdf">
-              <v-icon :x-small="$vuetify.breakpoint.smAndDown" small dark left v-html="icons.mdiFilePdfBox" />
-              PDF
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-
+          <v-btn :x-small="$vuetify.breakpoint.smAndDown" outlined color="deep-orange" v-if="urlAttachments" :href="urlAttachments">
+            <v-icon :x-small="$vuetify.breakpoint.smAndDown" small dark left v-html="icons.mdiDownload" />
+            приложения
+          </v-btn>
+          <v-btn :x-small="$vuetify.breakpoint.smAndDown" outlined color="deep-orange" v-if="urlPdf" :href="urlPdf">
+            <v-icon :x-small="$vuetify.breakpoint.smAndDown" small dark left v-html="icons.mdiFilePdfBox" />
+            PDF
+          </v-btn>
+        </v-card-actions>
       </v-card>
+
+    </v-card>
 
   </v-dialog>  
 </template>
@@ -71,7 +71,6 @@ import CopyButton from './CopyButton.vue';
 
 import {
   mdiClose,
-  mdiContentCopy,
   mdiDownload,
   mdiFilePdfBox,
   mdiSend,
@@ -91,7 +90,6 @@ export default {
     return {
       icons: {
         mdiClose,
-        mdiContentCopy,
         mdiDownload,
         mdiFilePdfBox,
         mdiSend,
