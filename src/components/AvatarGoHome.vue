6a80f57efc48062e463d185cf3f134144123dec6
Sergei Sokolov <hello@sergeisokolov.com>
2022-11-12 23:07:13 +0300

    Avatar with link home component

---
+++ src/components/AvatarGoHome.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-avatar
+    class="mr-3"
+    color="grey lighten-5"
+    size="70"
+    v-if="$vuetify.breakpoint.smAndUp"
+  >
+    <v-img
+      contain
+      max-height="70%"
+      :src="imgTreeJpg"
+      @click="goHome"
+      class="pointer"
+    ></v-img>
+  </v-avatar>
+</template>
+
+<script>
+import imgTreeJpg from '@/assets/tree.jpg';
+
+const { BASE_URL } = import.meta.env;
+
+export default {
+  data() {
+    return {
+      imgTreeJpg,
+    };
+  },
+  methods: {
+    goHome() {
+      window.location.href = BASE_URL;
+    },
+  },
+}
+</script>
+
+<style scoped>
+  .pointer {
+    cursor: pointer;
+  }
+</style>
